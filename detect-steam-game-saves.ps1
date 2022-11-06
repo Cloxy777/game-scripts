ff89a226f8a22f3cf288d8c22d2e149598fa4269
Cloxy777 <Kokorin1506@gmail.com>
2022-11-06 13:58:08 +0200

    added detect game save files base script

---
+++ detect-steam-game-saves.ps1
@@ -0,0 +1,16 @@
+﻿# detect-steam-game.ps1 Frostpunk
+$steamPath = 'E:\Games\Steam'
+
+$relativePaths = @{
+    'frostpunk'='\userdata\402463438\323190\remote\saves';
+}
+
+function Get-SavesPath{
+    param(
+        [Parameter(Mandatory=$true,position=0)] 
+        [ValidateSet('frostpunk')]
+        [string] $gameName)
+
+    $relativePath = $relativePaths[$gameName]
+    return $steamPath + $relativePath
+}
