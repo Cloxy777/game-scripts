56a7df7e2dbeefe706b83dc376aa8664e120b604
Cloxy777 <Kokorin1506@gmail.com>
2023-12-27 19:10:35 +0200

    TheyAreBillions

---
+++ TheyAreBillions/load_backup.ps1
@@ -0,0 +1,28 @@
+﻿$savesPath = 'C:\Users\Pavel\Documents\my games\They Are Billions'
+$backupPath = 'C:\Users\Pavel\Documents\my games\They Are Billions\auto-backups'
+
+
+
+# ex. load lastest      load_backup.ps1
+# ex. load by name      load_backup.ps1 0001
+
+$latestFolder = Get-ChildItem -Path $backupPath -Directory | 
+        Sort-Object LastWriteTime -Descending | 
+        Select-Object -First 1
+$backupName = $latestFolder.Name
+$sourcePath = "$backupPath\$backupName\Saves"
+
+# check if save exists.
+if (Test-Path $sourcePath -PathType Container) {
+    # Create the destination directory if it doesn't exist
+    if (-not (Test-Path $savesPath)) {
+        New-Item -ItemType Directory -Path $savesPath | Out-Null
+    }
+
+    # Copy the source directory to the destination directory
+    Copy-Item -Path $sourcePath -Destination $savesPath -Recurse -Force
+
+    Write-Host "Directory copied successfully."
+} else {
+    Write-Host "Source directory '$sourcePath' does not exist."
+}
