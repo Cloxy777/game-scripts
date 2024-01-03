156b216d793c559e985714590292d5f227c44097
Cloxy777 <Kokorin1506@gmail.com>
2024-01-03 21:23:32 +0200

    DontStarveTogether

---
+++ DontStarveTogether/invoker.ahk
@@ -0,0 +1,32 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+
+; ======== DELAY/SLEEP SETTINGS ===========
+
+KeyStateDelay := 25 ; Minimum delay between Key States (down or up)
+MenuDelay := 50 ; Min. Delay for switching between menus
+MinDelay := 25 ; Minimum delay between keystrokes
+
+; ======== Exit to Main Menu ===========
+
+#IfWinActive, Don't Starve Together
+
+F5::
+GoSub, Save
+Return
+
+F9::
+GoSub, Load
+Return
+
+; ========= SUBS ===========
+
+Save:
+	run, powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -File save_backup.ps1,, hide
+	Return
+	
+Load:
+	run, powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -File load_backup.ps1,, hide
+	Return
---
+++ DontStarveTogether/load_backup.ps1
@@ -0,0 +1,31 @@
+﻿$cluster = 'Cluster_1'
+$savesPath = "C:\Users\Pavel\Documents\Klei\DoNotStarveTogether\402463438"
+$fullSavesPath = "$($savesPath)\$($cluster)"
+$backupPath = "$($fullSavesPath)_auto_backups"
+
+
+# ex. load lastest      load_backup.ps1
+# ex. load by name      load_backup.ps1 0001
+
+$latestFolder = Get-ChildItem -Path $backupPath -Directory | 
+        Sort-Object LastWriteTime -Descending | 
+        Select-Object -First 1
+$backupName = $latestFolder.Name
+$sourcePath = "$backupPath\$backupName\$cluster"
+
+Remove-Item -Path $fullSavesPath -Recurse -Force
+
+# check if save exists.
+if (Test-Path $sourcePath -PathType Container) {
+    # Create the destination directory if it doesn't exist
+    if (-not (Test-Path $savesPath)) {
+        New-Item -ItemType Directory -Path $savesPath | Out-Null
+    }
+
+    # Copy the source directory to the destination directory
+    Copy-Item -Path $sourcePath -Destination $savesPath -Recurse -Force
+
+    Write-Host "Directory copied successfully."
+} else {
+    Write-Host "Source directory '$sourcePath' does not exist."
+}
