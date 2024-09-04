a6c76d4837599d8eb20372f08e3480a1bca314b1
Александр Александр <kokorin1506@gmail.com>
2024-09-04 12:56:31 +0300

    XCOM 2 War of the Chosen

---
+++ XCOM 2 War of the Chosen/save_backup.ps1
@@ -0,0 +1,27 @@
+﻿$savesPath = "C:\Users\Komre\OneDrive\Документы\My Games\XCOM2 War of the Chosen\XComGame\SaveData"
+$backupPath = "C:\Users\Komre\OneDrive\Документы\My Games\XCOM2 War of the Chosen\XComGame\backup"
+
+
+# ex. by timestamp     save_backup.ps1
+# ex. by name          save_backup.ps1 0001
+
+# use first argument or timestamp.
+$timeStamp = (Get-Date).ToString('yyyyMMdd_HHmmss')
+
+$backupName = "backup_$timeStamp"
+$destinationPath = "$backupPath\$backupName"
+
+# Check if the source directory exists
+if (Test-Path $savesPath -PathType Container) {
+    # Create the destination directory if it doesn't exist
+    if (-not (Test-Path $destinationPath)) {
+        New-Item -ItemType Directory -Path $destinationPath | Out-Null
+    }
+
+    # Copy the source directory to the destination directory
+    Copy-Item -Path $savesPath -Destination $destinationPath -Recurse -Force
+
+    Write-Host "Directory copied successfully."
+} else {
+    Write-Host "Source directory does not exist."
+}
