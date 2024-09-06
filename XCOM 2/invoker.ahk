ebb4aec2d46fc90a321561f316b26cce0cf4ec99
Александр Александр <kokorin1506@gmail.com>
2024-09-06 09:43:13 +0300

    XCOM 2

---
+++ XCOM 2/invoker.ahk
@@ -0,0 +1,32 @@
+﻿#NoEnv
+SendMode Input
+SetWorkingDir %A_ScriptDir%
+
+; ======== DELAY/SLEEP SETTINGS ===========
+
+KeyStateDelay := 25 ; Minimum delay between Key States (down or up)
+MenuDelay := 50 ; Min. Delay for switching between menus
+MinDelay := 25 ; Minimum delay between keystrokes
+
+; ======== Exit to Main Menu ===========
+
+#IfWinActive, XCOM 2
+
+F5:: Save()
+
+F9:: Load(1)
+F10:: Load(2)
+F11:: Load(3)
+F12:: Load(4)
+
+; ========= SUBS ===========
+
+Save() {
+    run, powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -File save_backup.ps1,, hide
+	Return
+}
+
+Load(Arg) {
+	run, powershell.exe -windowstyle hidden -ExecutionPolicy Bypass -File load_backup.ps1 -Save %Arg%,, hide
+	Return
+}
---
+++ XCOM 2/load_backup.ps1
@@ -0,0 +1,34 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [ValidateSet(1, 2, 3, 4, 5, 6, 7, 8)]
+    [string]$Save
+)
+
+
+$savesPath = "C:\Users\Komre\OneDrive\Документы\My Games\XCOM2\XComGame"
+$backupPath = "C:\Users\Komre\OneDrive\Документы\My Games\XCOM2\XComGame\backup"
+
+# ex. load lastest      load_backup.ps1
+# ex. load by name      load_backup.ps1 0001
+
+$latestFolder = Get-ChildItem -Path $backupPath -Directory | 
+        Sort-Object LastWriteTime -Descending | 
+        Select-Object -Index $($Save - 1)
+$backupName = $latestFolder.Name
+$sourcePath = "$backupPath\$backupName\SaveData"
+
+# check if save exists.
+if (Test-Path $sourcePath -PathType Container) {
+    # Create the destination directory if it doesn't exist
+    if (-not (Test-Path $savesPath)) {
+        New-Item -ItemType Directory -Path $savesPath | Out-Null
+    }
+
+    # Copy the source directory to the destination directory
+    Copy-Item -Path $sourcePath -Destination $savesPath -Recurse -Force
+
+    Write-Host "Directory copied successfully."
+} else {
+    Write-Host "Source directory '$sourcePath' does not exist."
+}
